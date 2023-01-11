5a9644694a4416b9fa07311b8c06cd31a197f038
maripper <80689424+maripper@users.noreply.github.com>
2023-01-11 07:22:21 -0300

    Create views.py

---
+++ base/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render
+
+import requests as req
+
+from django.http import JsonResponse,HttpResponse  
+
+import random
+
+from django.shortcuts import render
+
+from django.views.decorators.csrf import csrf_exempt
+
+import json
+
+import ast
+
+# perform business logic
+
+@csrf_exempt
+
+def welcome(request):
+
+    return render(request,"home.html",{})
+
+    # perform business logic
+
+@csrf_exempt
+
+def software_development(request):
+
+    return render(request,"software_development.html",{})
+
+@csrf_exempt
+
+def research(request):
+
+    return render(request,"research.html",{})
+
+@csrf_exempt
+
+def artistic(request):
+
+    return render(request,"artistic.html",{})
+
+@csrf_exempt  
+
+def start(request):
+
+    n = request.POST
+    return JsonResponse({'sender_and_receiver_comparing_sample_equivalent':'response'})
